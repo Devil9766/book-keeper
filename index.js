65e965bee4f1f4cfb660b6407e9112d8f69bf1b4
Devil9766 <ashasharma10883@gmail.com>
2024-02-15 11:49:02 +0530

    small changes in index.js -2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const db = new pg.Client({
 });
 
 
-const connectionString = "postgres://devil:H8bNa070MJ5QGE2OeCZxNcJyzdMQMOsK@dpg-cn65mhed3nmc739glb3g-a.oregon-postgres.render.com/booknotes";//you can create your postgreSQL server on render.com or Vercel and then they'll give u external URL copy that and paste it here
+const connectionString = "postgres://devil:H8bNa070MJ5QGE2OeCZxNcJyzdMQMOsK@dpg-cn65mhed3nmc739glb3g-a/booknotes";//you can create your postgreSQL server on render.com or Vercel and then they'll give u external URL copy that and paste it here
  
 const onlineDatabaseLink = new pg.Connection({
   connectionString: connectionString,
